31313ef2bcf0fd72e6e03cd0a9dadc4396e0359c
zhangbc <zhangbochengcheng189@163.com>
2017-05-21 23:12:29 +0800

    finished V1.0 fix print bug!

--- update_citycarrier.py
+++ update_citycarrier.py
@@ -41,7 +41,7 @@ class UpdateCityCarrier(IpProcessor):
         row_count = self.exec_no_query(insert_city_sql)
         if row_count:
             print u'表ip_city已插入{0}条记录！'.format(row_count)
-            
+
         # 更新成功，写入log
         query_city_sql = (r'''SELECT DISTINCT addr FROM ip_info '''
                            '''WHERE {0};'''.format(condition))
@@ -55,14 +55,18 @@ class UpdateCityCarrier(IpProcessor):
             update_log_sql = (r'''UPDATE ip_log_info SET city_count={0}, '''
                                '''city_finished=\'Y\' {1}''').format(city_count, condition_log)
             update_log_rows = self.exec_no_query(update_log_sql)
-            print u'表ip_log_info已更新{0}条记录！'.format(update_log_rows)
+
+            if update_log_rows:
+                print u'表ip_log_info已更新{0}条记录！'.format(update_log_rows)
         else:
             insert_log_sql = ('INSERT INTO ip_log_info('
                               'ip_range, city_count, city_finished, '
                               'carrier_count, carrier_finished) VALUES '
                               '(\'{0}.{1}.x.x\',{2},\'Y\',0,\'N\')').format(ia, ib, city_count)
             insert_log_rows = self.exec_no_query(insert_log_sql)
-            print u'表ip_log_info已插入{0}条记录！'.format(insert_log_rows)
+
+            if insert_log_rows:
+                print u'表ip_log_info已插入{0}条记录！'.format(insert_log_rows)
 
 
     def update_carrier(self, ia, ib):
@@ -95,14 +99,18 @@ class UpdateCityCarrier(IpProcessor):
             update_log_sql = (r'''UPDATE ip_log_info SET carrier_count={0}, '''
                                '''carrier_finished=\'Y\' {1}''').format(carrier_count, condition_log)
             update_log_rows = self.exec_no_query(update_log_sql)
-            print u'表ip_log_info已更新{0}条记录！'.format(update_log_rows)
+
+            if update_log_rows:
+                print u'表ip_log_info已更新{0}条记录！'.format(update_log_rows)
         else:
             insert_log_sql = (r'''INSERT INTO ip_log_info('''
                                '''ip_range, city_count, city_finished, '''
                                '''carrier_count, carrier_finished) VALUES '''
                                '''(\'{0}.{1}.x.x\',0,\'N\',{2},\'Y\')''').format(ia, ib, carrier_count)
             insert_log_rows = self.exec_no_query(insert_log_sql)
-            print u'表ip_log_info已插入{0}条记录！'.format(insert_log_rows)
+
+            if insert_log_rows:
+                print u'表ip_log_info已插入{0}条记录！'.format(insert_log_rows)
 
 
     def get_updated_list(self):
